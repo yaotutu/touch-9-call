c4c34acda94b799fb9ef19558a3ca3b0f1147f8e
yaotutu <957810668@qq.com>
2024-05-14 16:04:07 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import PWABadge from "./components/PWABadge.vue";
+</script>
+
+<template>
+  <h1>call2</h1>
+  <PWABadge />
+</template>
---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: '0.0.0.0'
+  },
+  plugins: [vue(), VitePWA({
+    strategies: 'injectManifest',
+    srcDir: 'src',
+    filename: 'sw.js',
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'touch-9-call',
+      short_name: 'call',
+      description: 'touch-9-call',
+      theme_color: '#ffffff',
+    },
+
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+    },
+
+    devOptions: {
+      enabled: true,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
